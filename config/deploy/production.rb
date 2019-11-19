57387bbf5707090a1ee4fd22f283811389c8cf43
suyashtripathi <suyashtripathi@gmail.com>
2019-11-19 15:19:28 +0530

    Testing with remove db role 19th Nov'19 3:19pm

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,7 +6,8 @@
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
-server '139.59.67.243', user: 'deploy', roles: %w{app db web}
+# server '139.59.67.243', user: 'deploy', roles: %w{app db web}
+server '139.59.67.243', user: 'deploy', roles: %w{app web}
 
 
 
