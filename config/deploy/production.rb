c725f5bd6369255b6ca34d7c0df5f4c6fba8efd3
suyashtripathi <suyashtripathi@gmail.com>
2019-11-19 15:31:15 +0530

    reverting removing the db role in production.rb 19 Nov'19 3:31pm

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,8 +6,7 @@
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
-# server '139.59.67.243', user: 'deploy', roles: %w{app db web}
-server '139.59.67.243', user: 'deploy', roles: %w{app web}
+server '139.59.67.243', user: 'deploy', roles: %w{app db web}
 
 
 
